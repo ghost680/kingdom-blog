6d6475007add2d1cbabace240565f5752ff88af0
wangpengkai <wangpengkai@deepexi.com>
2021-04-21 14:09:28 +0800

    增加查询方法

--- thinkphp5/tp5.dojo.org/app/index/controller/Index.php
+++ thinkphp5/tp5.dojo.org/app/index/controller/Index.php
@@ -9,8 +9,24 @@ class Index extends Controller
 {
     public function index()
     {
-        $result = Db::table('tp_user')->all();
+        // 查询所有数据
+        // $result = Db::table('tp_user')->all();
+        // 查询单个数据
+        // $result = Db::table('tp_user')->where('id', 1)->select();
+        // $result = Db::name('user')->where('id', 1)->select();
+        $result = db('user')->where('id', 1)->select();
+        $username = db('user')->where('id', 1)->value('username');
+        echo $username . '<br>';
         $this->assign('data', $result);
+        var_dump($result);
+        // 关联数据
+        // $age1 = array('Peter' => '35', 'Ben' => '37', 'Joe' => '43');
+        // $age2['Peter'] = "35";
+        // $age2['Ben'] = "37";
+        // $age2['Joe'] = "43";
+        // var_dump($result);
+        // var_dump($age1);
+        // var_dump($age2);
         return $this->fetch('index');
     }
 }
